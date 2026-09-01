5eaefed5d213001d015f481ecd593020c8f620c2
novimutiara67@gmail.com <novimutiara67@gmail.com>
2026-09-01 15:54:51 +0700

    Initial commit

---
+++ project-nilaiMahasiswa/script.js
@@ -0,0 +1,19 @@
+let makan = 20000;
+let transport = 15000;
+let minuman = 8000;
+
+let totalPengeluaran =makan + transport + minuman;
+
+let nama = "vibi";
+let mahasiswa = true;
+
+console.log("Pengeluaran Mahasiswa");
+console.log("Mahasiswa: " + mahasiswa);
+console.log("Nama: " + nama);
+
+console.log("Makan: " + makan);
+console.log("Transport: " + transport);
+console.log("Minuman: " + minuman);
+
+console.log("Total Pengeluaran: " + totalPengeluaran);
+
